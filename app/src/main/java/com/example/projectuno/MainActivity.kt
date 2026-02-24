f3bc8ee48d31edcd3cfcba47af1d50cfb8e18a61
User <maximusfrol2006@gmail.com>
2026-02-24 19:59:44 +0300

    .

--- app/src/main/java/com/example/projectuno/MainActivity.kt
+++ app/src/main/java/com/example/projectuno/MainActivity.kt
@@ -6,19 +6,26 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.fromColorLong
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.style.TextAlign
@@ -35,7 +42,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             ProjectUnoTheme {
-                MishinGlass()
+                MishinGlass5()
                 }
             }
         }
@@ -178,4 +185,27 @@ fun MishinGlass(){
         )
     }
 }
-//fewfef.CLMFROL3GRO;RG
+
+@Preview(showSystemUi = true)
+@Composable
+fun MishinGlass5(){
+    Card(
+        modifier = Modifier.background(color= Color.Gray)
+            .fillMaxWidth()
+            .padding(100.dp)
+    ){
+    }
+    Card(
+        modifier = Modifier.background(color = Color.Red)
+    ){
+
+    }
+}
+//Box(
+//modifier = Modifier.fillMaxSize(), // Заполнить весь экран
+//contentAlignment = Alignment.Center // Центрировать контент
+//) {
+//    Card {
+//        Text("По центру")
+//    }
+//}
