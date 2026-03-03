8d1ed20ed198f60db6c804a31f1f738d2759c48f
User <maximusfrol2006@gmail.com>
2026-03-03 21:38:29 +0300

    .

--- app/src/main/java/com/example/projectuno/MainActivity.kt
+++ app/src/main/java/com/example/projectuno/MainActivity.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.example.projectuno.module_2.MishinGlass5
 import com.example.projectuno.module_3.task_1.ListScroll
 import com.example.projectuno.module_3.task_1.itemList
 import com.example.projectuno.ui.theme.ProjectUnoTheme
