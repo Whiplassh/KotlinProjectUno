3ae41b1f6bdb1bf6f6312e6b9bb93a14ea5d5bd5
User <maximusfrol2006@gmail.com>
2026-03-03 20:31:09 +0300

    .

--- app/src/main/java/com/example/projectuno/module_3/task_1/ListCardUI.kt
+++ app/src/main/java/com/example/projectuno/module_3/task_1/ListCardUI.kt
@@ -1,11 +1,16 @@
 package com.example.projectuno.module_3.task_1
 
+import android.R
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -15,7 +20,13 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 
-
+val itemList = listOf(
+    ListItemsData(1, "Title 1 ", "Descr 1", R.drawable.ic_menu_camera),
+    ListItemsData(2, "Title 2 ", "Descr 2", R.drawable.ic_menu_edit),
+    ListItemsData(3, "Title 3 ", "Descr 3", R.drawable.ic_menu_rotate),
+    ListItemsData(4, "Title 4 ", "Descr 4", R.drawable.ic_menu_help),
+    ListItemsData(5, "Title 5 ", "Descr 5", R.drawable.ic_menu_delete)
+)
 @Composable
 fun ItemCard(item: ListItemsData) {
     Card(
@@ -46,6 +57,19 @@ fun ItemCard(item: ListItemsData) {
                     style = MaterialTheme.typography.bodyMedium
                 )
             }
+        }
+    }
+}
+
+@Composable
+fun ListScroll(items:List<ListItemsData>){
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize(),
+        contentPadding = PaddingValues(bottom = 16.dp)
+    ) {
+        items(items){
+                item -> ItemCard(item = item)
         }
     }
 }
